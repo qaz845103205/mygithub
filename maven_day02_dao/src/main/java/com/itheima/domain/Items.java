3e47357ccc21a8003824975994489d7730c87ac7
tanjian <845103205@qq.com>
2018-11-30 17:48:50 +0800

    git-提交2

---
+++ maven_day02_dao/src/main/java/com/itheima/domain/Items.java
@@ -0,0 +1,63 @@
+package com.itheima.domain;
+
+import java.util.Date;
+
+public class Items {
+    public Integer id;
+    public String name;
+    public Double price;
+    public String pic;
+    public Date createtime;
+    public String detail;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getPic() {
+        return pic;
+    }
+
+    public void setPic(String pic) {
+        this.pic = pic;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+
+
+}
